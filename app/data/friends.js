4107d06cad0f7c9b6128386cf102c9d7688f7e73
Vincent Nguyen <vnguyen2@gmail.com>
2016-11-04 21:43:57 -0500

    intial files and friends setup

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+
+var friendsArray = [
+	{
+		name: 'Mystery',
+		photo: 'http://lorempixel.com/output/people-q-c-400-300-7.jpg',
+		score: ["1", "2", "1", "2", "1", "2", "1", "2", "1", "1"]
+	},
+	{
+		name: 'Theresa',
+		photo: 'http://lorempixel.com/output/people-q-c-400-300-8.jpg',
+		score: ["4", "3", "4", "2", "5", "1", "3" ,"1" ,"2", "1"]
+	},
+	{
+		name: 'Angel',
+		photo: 'http://lorempixel.com/output/people-q-c-400-300-9.jpg',
+		score: ["5", "3", "5", "7", "5", "5", "2", "2", "5", "4"]
+	}
+];
+
+module.exports = friendsArray;
